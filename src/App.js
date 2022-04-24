085a990688158ceefafa8f73b393f1f0a0640098
namratagaddameedi414 <namrata.gaddameedi414@gmail.com>
2022-04-24 18:10:49 -0400

    commiting the files

---
+++ src/App.js
@@ -0,0 +1,170 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import fetchData from './services/rewardsService'
+import moment from 'moment';
+import _ from 'lodash';
+
+function App() {
+    const [data, setData] = useState([]);
+    const [currentSelected, setCurrentSelected] = useState(null);
+    const [filterPeriod] = useState(3);
+    const [permonthTransactions, setPerMonthTransactions] = useState([]);
+    
+    function getAbsoluteMonths(momentDate) {
+      var months = Number(momentDate.format("MM"));
+      var years = Number(momentDate.format("YYYY"));
+      console.log(months, years, months + (years * 12))
+      return months + (years * 12);
+    }
+    
+    function getListByFilterPeriod(res) {
+      console.log(res)
+      const filteredArr = res.filter(record=> {
+        const startDate = moment(record.transactionDate);
+        const endDate = moment(new Date());
+       
+        var startMonths = getAbsoluteMonths(startDate);
+        var endMonths = getAbsoluteMonths(endDate);
+        var monthDifference = endMonths - startMonths + 1;
+        if(monthDifference <= filterPeriod ){
+          return true
+        }
+        return false
+      });
+      return filteredArr;
+    }
+
+    const fetchDataRes=()=>{
+      return  fetchData.doSomething()
+              .then((res) => {
+                const filteredList = getListByFilterPeriod(res);
+                const collection = filteredList.map(record=> {
+                  let rewards = getRewards(record.amount);
+                  let transactionDate = moment(record.transactionDate).format("MM/DD/YYYY");
+                  let month = moment(record.transactionDate).format("MMMM");
+                  return { ...record,rewards, transactionDate, month }
+                });
+                var grouped = _.groupBy(collection, function(item) {
+                    return item.name;
+                });
+                const filteredOt = _.map(grouped, function(value, key){
+                  const transList= [];
+                  const userTransactions ={
+                    totalAmount: 0,
+                    totalRewards: 0,
+                  }
+                  const transactionMonthList= _.groupBy(value, function(item) {
+                    return moment(item.transactionDate).format("MMMM");
+                  });
+                  for (const key in transactionMonthList) {
+                    const val = transactionMonthList[key];
+                    const monthlyTransactions ={
+                      transListByMonth: val,
+                      totalAmount: 0,
+                      totalRewards: 0,
+                      month: key
+                    };
+                    val.map(x=> {
+                    monthlyTransactions.totalAmount += x.amount;
+                    monthlyTransactions.totalRewards += x.rewards;
+                    });
+                    transList.push(monthlyTransactions); 
+                    userTransactions.totalAmount +=  monthlyTransactions.totalAmount;
+                    userTransactions.totalRewards +=  monthlyTransactions.totalRewards;
+                  };
+                  return {transList, name: key, ...userTransactions };
+                });
+
+               return filteredOt;
+              })
+              .then(result=> setData(result))
+              .catch((err)=> console.log(err))
+              
+    }
+
+    function getRewards(val){
+      let rewardPoint = 0;
+      let plus100 = val - 100;
+      let plus50 = val - 50;
+      if(plus100 > 0) {
+        rewardPoint += plus100*2;
+      }
+      if(plus50 > 0) {
+        rewardPoint += 50*1
+      }
+      return parseInt(rewardPoint)
+    }
+
+    const onRowClick = (row, name, index) => {
+      const val = name+index;
+      setCurrentSelected(val);
+      setPerMonthTransactions(row);
+    }
+
+    useEffect(()=>{
+      fetchDataRes();
+    }, []);
+
+    return (
+      <div className="App">
+            <div className="contentcontainer">
+                <h1>Reward Point List</h1>
+                {
+                  !data && (
+                    <h5>...Loading</h5>
+                  )
+                }
+                {data && 
+                  <table style={{minWidth: "600px"}} data-testid="reward-list">
+                    <tr>
+                      <th></th>
+                      <th>Customer</th>
+                      <th>Month</th>
+                      <th>Amount</th>
+                      <th>Reward Points</th>
+                    </tr>
+                    { data && data.length>0 && data.map((record, idx) =>
+                        record && record.transList.map((row,index)=> 
+                        <>
+                            <tr key={`row-${record.name}${index}`} data-testid={`row-${record.name}${index}`}>
+                              <td style={{fontWeight:'bolder', fontSize:'18px', padding:'10px'}} onClick={() => onRowClick(row.transListByMonth,record.name,index )}>+</td>
+                              <td> {record.name}</td>
+                              <td>{row.month}</td>
+                              <td> ${row.totalAmount}</td>
+                              <td style={{color:'red', fontWeight: 'bold'}}>{row.totalRewards}</td>
+                            </tr>
+                            { currentSelected && currentSelected === `${record.name}${index}` && 
+                                <tr>
+                                  <td colSpan={5}>
+                                  <table style={{width:'100%'}}>
+                                    <tr style={{backgroundColor: 'wheat'}}>
+                                      <th>TransactionDate</th>
+                                      <th>Rewards</th>
+                                      <th>Amount</th>
+                                    </tr>
+                                  {  permonthTransactions.map(monthTransaction => 
+                                    <tr key={record.tId} style={{backgroundColor:'white'}}>
+                                      <td>{monthTransaction.transactionDate}</td>
+                                      <td> ${monthTransaction.amount}</td>
+                                      <td>{monthTransaction.rewards}</td>
+                                    </tr>
+                                  )}
+                                  </table>
+                                  </td>
+                                </tr> 
+                            }
+                        </>
+                        )
+                      )}
+                      {
+                        data.length === 0 && 
+                        <tr colSpan="5">No record found</tr>
+                      }
+                  </table>
+                }
+            </div>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render, fireEvent, act, waitFor } from '@testing-library/react';
+import fetchData from './services/rewardsService'
+
+describe('my function or component', () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+  
+  afterEach(() => {
+    document.body.removeChild(container);
+    ReactDOM.unmountComponentAtNode(container);
+    container = null;
+  });
+  it('renders without crashing', async() => {
+
+      const callApi = jest.spyOn(fetchData, 'doSomething').mockReturnValue(Promise.resolve(
+      [
+        {
+          "id": 0,
+          "name": "Shelia Goodman",
+          "amount": 606.91,
+          "transactionDate": "Tue Mar 15 2022 07:51:39 GMT-0400 (Eastern Daylight Time)",
+          "tId": 1
+        },
+        {
+          "id": 1,
+          "name": "Pena Chase",
+          "amount": 299.71,
+          "transactionDate": "Mon Mar 14 2022 22:44:59 GMT-0400 (Eastern Daylight Time)",
+          "tId": 2
+        },
+        {
+          "id": 2,
+          "name": "Graham Jacobs",
+          "amount": 324.94,
+          "transactionDate": "Tue Feb 01 2022 12:52:42 GMT-0500 (Eastern Standard Time)",
+          "tId": 3
+        }
+      ]));
+      const {getAllByTestId } = render(<App />);
+      expect(await waitFor(() => getAllByTestId(/^row-*/).length)).toBe(3);
+      // expect((await waitFor(() => getByTestId('reward-list'))).children).toHaveLength(3);
+      // console.log(listNode.children)
+      // expect(listNode.children).toHaveLength(3);
+  });
+
+  it('should show Loading when there is no data', async() => {
+    const div = document.createElement('div');
+    const callApi = jest.spyOn(fetchData, 'doSomething').mockReturnValue(Promise.resolve([]));
+    const {queryByText} = render(<App />);
+    expect(await waitFor(() => queryByText('No record found'))).toBeTruthy();
+    // ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should show Loading when there is no dataxxxxx', async() => {
+    const callApi = jest.spyOn(fetchData, 'doSomething').mockReturnValue(Promise.resolve(
+      [
+        {
+          "id": 0,
+          "name": "Shelia Goodman",
+          "amount": 606.91,
+          "transactionDate": "Tue Mar 15 2022 07:51:39 GMT-0400 (Eastern Daylight Time)",
+          "tId": 1
+        },
+        {
+          "id": 1,
+          "name": "Pena Chase",
+          "amount": 299.71,
+          "transactionDate": "Mon Mar 14 2022 22:44:59 GMT-0400 (Eastern Daylight Time)",
+          "tId": 2
+        },
+        {
+          "id": 2,
+          "name": "Graham Jacobs",
+          "amount": 324.94,
+          "transactionDate": "Tue Feb 01 2022 12:52:42 GMT-0500 (Eastern Standard Time)",
+          "tId": 3
+        }
+      ]));
+      const {getAllByTestId, getAllByText, queryByText } = render(<App />);
+      const firstId = await waitFor(() => getAllByText("+"));
+      act(() =>{
+        fireEvent.click(firstId[0]);
+        // expect(getAllByText(/TransactionDate/i)).toBeTruthy();
+      });
+      expect(await waitFor(() => getAllByText(/TransactionDate/i))).toBeTruthy();
+  
+  });
+});
+
+
+
